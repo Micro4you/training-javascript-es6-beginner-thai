e1875887ebe4b6ab92c606a2214e9cc61e461b0d
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-04-28 14:03:36 +0700

    Add example of arrow function that return pure simple object

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 'use strict'
 
+console.log('## Simple function with no parameter');
 // let sayHi = function(){
 //   console.log('Hello, Pon!');
 // }
 
 let sayHi = () => console.log('Hello, Pon!');
-
 sayHi();
 
 
+console.log('## Simple function with a parameter');
 // let printText = function(message){
 //   console.log(message);
 // }
 
 let printText = message => console.log(message);
-
 printText('Teerasej');
 
 
+console.log('## Simple function with multiple parameters');
 // let plus = function(first, second){
 //   return first + second;
 // }
 
 let plus = (first,second) => first + second;
-
 console.log(plus(1,2));
+
+
+
+console.log('## Simple function that return object');
+// let createUser = function(){
+//   return { name:'Pon'};
+// }
+
+let createUser = () => (){ name:'Pon' });
+
+let result = createUser();
+console.log(result.name);
 
 
 
