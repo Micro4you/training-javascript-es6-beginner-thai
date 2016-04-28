7dcd535b36f619a280994b226645c6377e8bdf85
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-04-28 17:33:22 +0700

    Add example block, according to finish template

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 'use strict'
+
+console.log('## Simple function with no parameter');
+// let sayHi = function(){
+//   console.log('Hello, Pon!');
+// }
+
+
+
+// console.log('## Simple function with a parameter');
+// let printText = function(message){
+//   console.log(message);
+// }
+
+
+
+// console.log('## Simple function with multiple parameters');
+// let plus = function(first, second){
+//   return first + second;
+// }
+
+
+
+// console.log('## Simple function that return object');
+// let createUser = function(){
+//   return { name:'Pon'};
+// }
+
+
+
+// let result = createUser();
+// console.log(result.name);
+
+
+
+// console.log('Start counting...');
+// let amount = 0;
+
+// setInterval(function addMore() {
+//     amount++;
+//     console.log(amount);
+// }, 1000)
