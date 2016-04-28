c4a65f32683e74d28270e902c185a8a5ebc2a684
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-04-28 12:43:12 +0700

    Add 3 example for: - simple arrow function, - multiple parameter arrow function - function expression arrow function

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 'use strict'
+
+// let printText = function(message){
+//   console.log(message);
+// }
+
+let printText = message => console.log(message);
+
+printText('Teerasej');
+
+
+// let plus = function(first, second){
+//   return first + second;
+// }
+
+let plus = (first,second) => first + second;
+
+console.log(plus(1,2));
+
+
+let amount = 0;
+
+// setInterval(function addMore() {
+//     amount++;
+//     console.log(amount);
+// }, 1000)
+
+setInterval(() => {
+  amount++;
+  console.log(amount);
+},1000);
