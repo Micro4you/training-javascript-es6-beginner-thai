a6e6f9acb5b011122cf1de21e1ac956dc25853a1
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-09-17 16:09:18 +0700

    Add arrow function practice

---
+++ function/arrow_function/app-finish.js
@@ -0,0 +1,40 @@
+'use strict'
+
+// let plusES5 = function(a, b){
+    
+//     console.log("A:", a, ", B:", b);
+//     let result = a + b;
+//     return result;
+
+// };
+
+// console.log(plusES5(9,10));
+
+// ES6 style (arrow function)
+// let plusES6 = (a,b) => {
+//     console.log("A:", a, ", B:", b);
+//     let result = a + b;
+//     return result;
+// }
+
+// console.log(plusES6(9,10));
+
+// Arrow function with one parameter
+// let hello = (name) => {
+//     console.log("Hi,", name);
+// }
+// let hello = name => {
+//     console.log("Hi,", name);
+// }
+// hello("Pon");
+
+// Arrow function wihtout parameter 
+// let hiES6 = () => {
+//     console.log('Hi, ES6');
+// }
+
+// let hiES6 = () => console.log("Hi, ES6");
+// hiES6();
+
+// let hiES6 = () => { return "Hi, ES6" };
+// console.log(hiES6());
---
+++ function/arrow_function/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+
+
+
+// ES6 style (arrow function)
+
+
+
+// Arrow function with one parameter
+
+
+
+// Arrow function wihtout parameter 
+
+
