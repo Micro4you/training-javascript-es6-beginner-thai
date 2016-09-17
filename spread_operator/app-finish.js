a96d590275584200d087e1adc34c7b1864ad6af5
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-09-17 15:49:37 +0700

    Add spread operator with array

---
+++ spread_operator/app-finish.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let levelOnePokemons = ['Pikachu','Magikarp','Balbasaur'];
+
+let allPokemons = ['Meowth', 'Poliwag', ...levelOnePokemons];
+
+console.log(allPokemons);
---
+++ spread_operator/app-literals.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let levelOnePokemons = ['Pikachu','Magikarp','Balbasaur'];
+
+
+
+console.log('');
