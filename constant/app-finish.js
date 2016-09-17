1166583ae2c462c08f67474090e24cebc29026e8
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-09-17 14:58:56 +0700

    Add constant practice

---
+++ constant/app-finish.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const WEB_API_URL = "http://www.nextflow.in.th";
+
+// This cause TypeError!
+WEB_API_URL = "facebook.com/nextflow";
+
+console.log(WEB_API_URL);
---
+++ constant/app.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let WEB_API_URL = "http://www.nextflow.in.th";
+
+
+
+console.log(WEB_API_URL);
