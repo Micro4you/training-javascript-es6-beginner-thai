06396c63351b68f62830ba7b391f35823067d7b7
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-09-19 18:02:21 +0700

    Add string interpolation

---
+++ string/app-finish.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let username = "Teerasej";
+let pokemonName = "Pikachu";
+
+// Use `` and ${} to insert string to text
+console.log( `${username}, you choose ${pokemonName}!`);
---
+++ string/app.js
@@ -0,0 +1,6 @@
+'use strict'
+
+let username = "Teerasej";
+let pokemonName = "Pikachu";
+
+console.log();
