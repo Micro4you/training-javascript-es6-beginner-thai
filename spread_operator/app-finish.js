9d68a8e39598e53cee8c7ba26ab6096f9dfdd219
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-09-19 17:52:49 +0700

    Add comment for explain step

--- spread_operator/app-finish.js
+++ spread_operator/app-finish.js
@@ -2,11 +2,14 @@
 
 let levelOnePokemons = ['Pikachu','Magikarp','Balbasaur'];
 
+// Insert array to another array
 let allPokemons = ['Meowth', 'Poliwag', ...levelOnePokemons];
 
 console.log(allPokemons);
 
 let pikachuName = "Pikachu";
+
+// Split into literal array with spread operator
 let arrayWords = [...pikachuName];
 
 console.log(arrayWords);
