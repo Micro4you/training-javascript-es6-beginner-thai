cb0e1fdeda5d3605974d3e75d0d09b5df6d00734
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-09-19 17:57:27 +0700

    Add arrow function body

---
+++ function/arrow_function_body/app-finish.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let pokemons = ['Pikachu','Magikarp','Balbasaur'];
+
+// ES 5
+// pokemons.forEach(function(mon){
+//     console.log(mon);
+// });
+
+// ES 6
+pokemons.forEach(mon => console.log(mon));
---
+++ function/arrow_function_body/app.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let pokemons = ['Pikachu','Magikarp','Balbasaur'];
+
+// ES 5
+
+
+// ES 6
