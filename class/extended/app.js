4fa9b2819f4ac051272fc42b54e234a56df5580e
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-10-24 13:43:43 +0700

    Show starter for practice extend keyword

--- class/extended/app.js
+++ class/extended/app.js
@@ -1,2 +1,15 @@
 'use strict'
 
+class Ball {
+
+   constructor(name) {
+       this.name = name;
+   }
+
+   catchMonster() {
+       console.log(`${this.name} threw.`);
+   }
+}
+
+let monBall = new MonBall();
+mokeBall.catchMonster();
