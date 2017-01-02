63bf14fa54e962bfc8d63cf7400552f5ba67afce
Teerasej Jiraphatchandej <teerasej@gmail.com>
2017-01-02 23:03:33 +0700

    Update to avoid problem in copyright

--- function/arrow_function_body/app-finish.js
+++ function/arrow_function_body/app-finish.js
@@ -1,11 +1,11 @@
 'use strict'
 
-let pokemons = ['Pikachu','Magikarp','Balbasaur'];
+let monsters = ['Slime','Orc','Skeleton Warrior'];
 
 // ES 5
-// pokemons.forEach(function(mon){
+// monsters.forEach(function(mon){
 //     console.log(mon);
 // });
 
 // ES 6
-pokemons.forEach(mon => console.log(mon));
+monsters.forEach(mon => console.log(mon));
