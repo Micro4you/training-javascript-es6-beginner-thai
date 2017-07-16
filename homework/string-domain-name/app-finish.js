fd38f2ff2bd14bbbbb786f88f639ad435a16e0e2
Teerasej <teerasej@gmail.com>
2017-07-16 14:05:33 +0700

    Add string object homework

---
+++ homework/string-domain-name/app-finish.js
@@ -0,0 +1,19 @@
+
+
+// let website = "https://www.nextflow.in.th";
+let website = "http://www.bots.in.th";
+
+// -> https://, http://
+let fromIndex = 0;
+let toIndex = website.lastIndexOf('/') + 1;
+let protocol = website.slice(fromIndex, toIndex);
+
+// -> nextflow.in.th, bots.in.th
+fromIndex = website.indexOf(".") + 1;
+toIndex = website.length;
+let domainName = website.slice( fromIndex, toIndex);
+
+console.log("============");
+console.log('Protocal:', protocol);
+console.log('Domain name:', domainName);
+console.log("============");
---
+++ homework/string-domain-name/app.js
@@ -0,0 +1,15 @@
+
+
+let website = "https://www.nextflow.in.th";
+// let website = "http://www.bots.in.th";
+
+// -> https://, http://
+let protocol;
+
+// -> nextflow.in.th, bots.in.th
+let domainName;
+
+console.log("============");
+console.log('Protocal:', protocol);
+console.log('Domain name:', domainName);
+console.log("============");
