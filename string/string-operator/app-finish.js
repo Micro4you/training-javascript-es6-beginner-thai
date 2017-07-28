b40d26152ed4208c450d8c6eb31e73c87c2c792a
Teerasej <teerasej@gmail.com>
2017-07-28 08:17:57 +0700

    Add for string operator practice

---
+++ string/string-operator/app-finish.js
@@ -0,0 +1,14 @@
+
+
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName + " " + lastName);
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+let email = "training";
+email += "@nextflow.in.th";
+console.log(email);
