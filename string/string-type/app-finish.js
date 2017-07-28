d154f801f3805b13f6f92bb4ca315334930ab1d1
Teerasej <teerasej@gmail.com>
2017-07-28 08:19:40 +0700

    easier to understand

--- string/string-type/app-finish.js
+++ string/string-type/app-finish.js
@@ -6,10 +6,11 @@ let website = new String("http://www.nextflow.in.th");
 console.log(name, website);
 
 // Length of string
-console.log(name.length);
+console.log(website.length);
 
 // CharAt
-console.log("Character at: ", website.charAt(0));
+console.log("The First Character : ", website.charAt(0));
+console.log("Character at the end: ", website.charAt(website.length - 1));
 
 // index of 
 let index = website.indexOf("www");
@@ -18,10 +19,14 @@ console.log("Index of 'www.':", index);
 let lastIndex = website.lastIndexOf(".");
 console.log("Last index of '.'", lastIndex);
 
+
 // slice
+console.log('==== Slice =====')
 // website = "https://www.bots.in.th";
-let fromIndex = website.indexOf(".") + 1;
+
+let fromIndex = website.indexOf(".");
 let toIndex = website.indexOf(".in.th");
+console.log('Slice from index: ', fromIndex, 'to index:', toIndex );
 
 let sliceWord = website.slice(fromIndex, toIndex);
 console.log("Slice word:", sliceWord);
